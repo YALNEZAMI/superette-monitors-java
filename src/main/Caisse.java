1ae718a721c1a06c24628c9cede6fe489c0d3799
YASER ALNEZAMI <yaseralnezami@gmail.com>
2025-11-25 23:34:46 +0100

    feat: implement synchronized cash register operations
    This commit introduces synchronization to the cash register operations to ensure thread safety. It includes synchronized methods for paying, entering the cash register, depositing products, and taking products by the cashier. It also addresses the synchronization for clients leaving the cash register.  This prevents race conditions and ensures data consistency when multiple clients and cashiers interact with the same cash register.

--- src/main/Caisse.java
+++ src/main/Caisse.java
@@ -12,6 +12,7 @@ public class Caisse {
     private Client currentClient = null;
     private boolean clientDone = true;
     private double chiffreDaffaire = 0;
+    private boolean paid = false;
 
     private void incCurrentClientCase() {
         if (currentClientCase == SIZE - 1) {
@@ -29,15 +30,22 @@ public class Caisse {
         }
     }
 
-    private void payer(Client client) {
+    synchronized public void payer(Client client) {
+        while (!isEmpty()) {
+            try {
+                wait();
+            } catch (Exception e) {
+            }
+        }
         int somme = 0;
         for (Product product : scannedProducts) {
             somme += product.getPrice();
         }
         chiffreDaffaire += somme;
-        System.out.println(somme + "payé par :" + client.getName());
-        currentClient = null;
+        System.out.println(somme + " payé par :" + client.getName());
+        paid = true;
         scannedProducts.clear();
+        notify();
     }
 
     private boolean isEmpty() {
@@ -48,7 +56,19 @@ public class Caisse {
                 break;
             }
         }
-        System.out.println("tapis vide? :" + b);
+        // System.out.println("tapis vide? :" + b);
+        return b;
+    }
+
+    private boolean isFull() {
+        boolean b = true;
+        for (int i = 0; i < tapis.length; i++) {
+            if (tapis[i] == null) {
+                b = false;
+                break;
+            }
+        }
+        System.out.println("tapis full? :" + b);
         return b;
     }
 
@@ -63,39 +83,74 @@ public class Caisse {
             } catch (Exception e) {
             }
         }
+        System.out.println(client.getName() + " entre en caisse");
         currentClient = client;
+        notifyAll();// reveiller le caissier car attend le client d'entrer
     }
 
-    synchronized public void deposer(Client client, Product product, boolean lastProduct) {
-        while (!client.equals(currentClient) || tapis[currentClientCase] != null) {
+    synchronized public void sortirCaisse(Client client) {
+        while (!paid) {
             try {
                 wait();
             } catch (Exception e) {
             }
+        }
+        paid = false;
+        currentClient = null;
+        clientDone = false;
+        System.out.println(client.getName() + " sort de caisse");
+        notifyAll();
+    }
+
+    synchronized public void deposer(Client client, Product product, boolean lastProduct) {
+        while (isFull()) {
+            try {
+                wait();
+            } catch (Exception e) {
+            }
+        }
+        if (lastProduct) {
+            System.out.println("Client---" + client.getName() + " deposer le dernier article " + product.getName());
+
+        } else {
+            System.out.println("Client---" + client.getName() + " deposer " + product.getName());
+
         }
         tapis[currentClientCase] = product;
         incCurrentClientCase();
         if (lastProduct) {
             this.clientDone = true;
+
         }
     }
 
-    synchronized public void prendre() {
-        while ((tapis[currentCaissierCase] == null && !clientDone) || currentClient == null) {
+    synchronized public void prendre(Caissier caissier) {
+        while ((isEmpty() && !clientDone) || currentClient == null) {
             try {
-                System.out.println("caissier attend car tapis vide mais client n' pas encore fini");
+                if (isEmpty() && !clientDone) {
+                    System.out.println("Caissier---" + caissier.getName()
+                            + " attend car tapis vide mais client n' pas encore fini");
+
+                } else if (currentClient == null) {
+                    System.out.println("Caissier---" + caissier.getName() + " attend car pas de client");
+                }
                 wait();
             } catch (Exception e) {
             }
         }
-        System.out.println(
-                "caissier prend " + tapis[currentCaissierCase].getName() + " du client " + currentClient.getName());
-        scannedProducts.add(tapis[currentCaissierCase]);
-        tapis[currentCaissierCase] = null;
-        incCurrentCaissierCase();
-        if (clientDone && isEmpty()) {
+        if (tapis[currentCaissierCase] != null) {
+            System.out.println(
+                    "Caissier--- " + caissier.getName() + " prend "
+                            + tapis[currentCaissierCase].getName() + " du client " + currentClient.getName());
+            scannedProducts.add(tapis[currentCaissierCase]);
+            tapis[currentCaissierCase] = null;
+            incCurrentCaissierCase();
+            notifyAll();// tapis avec case libre
+        }
+        if (!paid) {
             payer(currentClient);
         }
+
     }
 
     @Override
