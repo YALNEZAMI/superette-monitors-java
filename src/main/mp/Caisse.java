1d9cd89ba0fa759912477868799fe6dad7a08653
YASER ALNEZAMI <yaseralnezami@gmail.com>
2025-11-29 14:18:27 +0100

    feat: implement facturation system for Caisse
    This commit introduces a facturation system to the `Caisse` class.  It replaces the previous `payer` method with a `facturer` method that stores the scanned products for each client in a `facturator` map.  The `toString` method is updated to display the facturation information, including the total revenue.  The logic for notifying the client when they can leave the caisse has also been improved.

--- src/main/mp/Caisse.java
+++ src/main/mp/Caisse.java
@@ -1,7 +1,11 @@
 package main.mp;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import main.products.Product;
 import main.threads.Caissier;
@@ -14,9 +18,9 @@ public class Caisse {
     private List<Product> scannedProducts = new ArrayList<Product>();
     private Client currentClient = null;
     private boolean clientDone = true;
-    private double chiffreDaffaire = 0;
     private boolean paid = false;
     private boolean isJobDone = false;
+    private Map<Client, List<Product>> facturator = new HashMap<>();
 
     private void incCurrentClientCase() {
         if (currentClientCase == Superette.SIZE_CAISSE_TAPIS - 1) {
@@ -34,22 +38,11 @@ public class Caisse {
         }
     }
 
-    synchronized public void payer(Client client) {
-        while (!isEmpty()) {
-            try {
-                wait();
-            } catch (Exception e) {
-            }
-        }
-        double somme = 0;
-        for (Product product : scannedProducts) {
-            System.out.println("pricing: " + product.getPrice());
-            somme += product.getPrice();
-        }
-        chiffreDaffaire += somme;
-        System.out.println(somme + " payé par :" + client.getName());
+    synchronized public void facturer(Client client) {
+        // enregistrer dans les facturation
+        facturator.put(client, List.copyOf(scannedProducts));
         paid = true;
-        notify();
+        notifyAll();// notifier le client qu'il peut sortir de la casise
     }
 
     private boolean isEmpty() {
@@ -74,10 +67,6 @@ public class Caisse {
         }
         System.out.println("tapis full? :" + b);
         return b;
-    }
-
-    public double getChiffreDaffaire() {
-        return chiffreDaffaire;
     }
 
     synchronized public void entrerEnCaisse(Client client) {
@@ -136,6 +125,12 @@ public class Caisse {
     }
 
     synchronized public void prendre(Caissier caissier) {
+        /**
+         * on attend si il reste des client dans le magasin et
+         * soit il n'y a pas de client à la caisse (mais il en reste dans les raysons
+         * ou à la pile de chariots )
+         * soit il y a un client mais il n'a pas encore fini de déposer ses articles
+         */
         while (((isEmpty() && !clientDone) || currentClient == null) && !isJobDone) {
             try {
                 if (isEmpty() && !clientDone) {
@@ -149,6 +144,7 @@ public class Caisse {
             } catch (Exception e) {
             }
         }
+        // si tous les client sont sorti --> rien à faire
         if (!isJobDone) {
             if (tapis[currentCaissierCase] != null) {
                 System.out.println(
@@ -157,10 +153,10 @@ public class Caisse {
                 scannedProducts.add(tapis[currentCaissierCase]);
                 tapis[currentCaissierCase] = null;
                 incCurrentCaissierCase();
-                notifyAll();// tapis avec case libre
+                notifyAll();// notifie le client à la caisse qu'il peut déposer
             }
-            if (!paid) {
-                payer(currentClient);
+            if (!paid && clientDone && isEmpty()) {// payé si tous les produits ont été déposé et scanné
+                facturer(currentClient);
             }
         }
 
@@ -168,26 +164,61 @@ public class Caisse {
 
     @Override
     public String toString() {
-        String s = "Scanned products:\n";
-        for (Product product : scannedProducts) {
-            s += product.getName() + "\n ";
-        }
+        String s = "";
+
         if (currentClient != null) {
             s += "Client actuel: \n" + currentClient.getName();
+            s += " qui ";
             if (clientDone) {
-                s += " (done)\n";
-                if (!paid) {
-                    s += "client attend pour paiment";
+                if (paid) {
+                    s += "a fini de déposé mais n'a pas encore payé\n";
+                } else {
+                    s += "a fini de déposé et a payé\n";
+
                 }
             } else {
-                s += " (not done)\n";
+                s += "est toujours en train de déposer ses articles.\n";
+            }
+            s += "Articles déposés: \n";
+            for (Product product : scannedProducts) {
+                s += product.getName() + "\n ";
             }
 
         } else {
-            s += "Client actuel: abscent\n";
+            s += "Client actuel: aucun\n";
 
         }
-        s += "Chiffre d'affaire : " + chiffreDaffaire + "\n";
+        s += "\n";
+
+        s += "Facturation: \n";
+        s += "\n";
+        double chiffreDaffaire = 0;
+        for (Client client : facturator.keySet()) {
+            s += "Client: " + client.getName() + "\n";
+            s += "Articles: \n";
+            double sommeParClient = 0;
+            if (facturator.get(client).size() == 0) {
+                s += "Aucun article\n";
+            }
+            // int nbrItems=0;
+            for (Product product : facturator.get(client)) {
+                s += product.getName() + " " + product.getPrice() + "$\n";
+                sommeParClient += product.getPrice();
+
+            }
+
+            BigDecimal truncatedDouble = new BigDecimal(sommeParClient);
+            truncatedDouble = truncatedDouble.setScale(2, RoundingMode.HALF_DOWN);
+            chiffreDaffaire += truncatedDouble.doubleValue();
+
+            s += "Total facture: " + truncatedDouble + "$\n";
+            s += "\n";
+
+        }
+        s += "\n";
+        s += "\n";
+        s += "Chiffre d'affaire: " + chiffreDaffaire + "$\n";
+
         return s;
     }
 
