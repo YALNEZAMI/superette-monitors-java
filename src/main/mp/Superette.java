08e9859459c803202bd3be6b1a62e58a28989b01
YASER ALNEZAMI <yaseralnezami@gmail.com>
2025-11-29 14:17:26 +0100

    feat: add constants and comments for better readability and maintainability

--- src/main/mp/Superette.java
+++ src/main/mp/Superette.java
@@ -3,15 +3,29 @@ package main.mp;
 import main.threads.Client;
 
 public class Superette {
-    public static String[] availableProductsNames = { "beurre", "farine", "sucre", "lait" };// à synchroniser avec les
-                                                                                            // sous type de produit et
-                                                                                            // ProductFactory
-    public static int NBR_CHARIOTS = 5;
+    /* à synchroniser avec le switch de ProductFactory */
+    public static String[] availableProductsNames = { "beurre", "farine", "sucre", "lait" };
+    /* Nombre de chariots initial */
+    public static int NBR_CHARIOTS = 3;
+    /**
+     * le maximum qu'un client peut convoiter d'une catégorie donné
+     * exemple autorisé avec 5: 5 farine, 0 sucre, 3 beurre, 1 lait
+     * exemple interdit avec 5: 0 farine, 2 sucre, 8 beurre, 6 lait
+     */
     public static int MAX_PRODUCT_BY_CATEGORY_PER_CLIENT = 5;
+    /** le nombre de clients dans le supermarché */
     public static int NBR_INIT_CLIENTS = 2;
+    /* Le nombre maximum qu'un rayon peut contenir de produits */
     public static int MAX_SIZE_RAYON = 6;
+    /** La taille du tapis de la caisse */
     public static int SIZE_CAISSE_TAPIS = 5;
+    /** Capacité du chef de rayon par catégorie de produit */
     public static int MAX_CHEF_RAYON_CAPACITY = 5;
+    // les temps d'attente en ms
+    public static int TEMPS_CHEF_RAYON_FAIRE_PLEIN = 500;
+    public static int TEMPS_CHEF_RAYON_ENTRE_RAYONS = 200;
+    public static int TEMPS_CLIENT_ENTRE_RAYONS = 300;
+    public static int TEMPS_CLIENT_DEPOSE_PRODUIT_SUR_TAPIS = 20;
 
     private RangeeChariots rangeeChariots;
     private RangeeRayons rangeeRayons;
@@ -25,7 +39,6 @@ public class Superette {
     }
 
     public int getNbrClientSortie() {
-        // System.out.println("nbr clients sortie" + nbrClientSortie);
         return nbrClientSortie;
     }
 
@@ -48,8 +61,11 @@ public class Superette {
     synchronized public void sortir(Client client) {
         nbrClientSortie++;
         System.out.println(client.getName() + " est sorti du supermarché");
-        // pour que le dernier client qui sort reveille le caissier(et donc il arrete de
-        // travailler)
+        /*
+         * pour que le dernier client qui sort reveille le caissier(et donc il arrete de
+         * travailler)
+         * pas besoin de réveiller le chef de rayon car il n'attend jamais
+         */
         if (nbrClientSortie == Superette.NBR_INIT_CLIENTS) {
             caisse.reveillerCaissierPourRentrer();
         }
@@ -58,10 +74,10 @@ public class Superette {
     @Override
     public String toString() {
         String s = "Superette: \n";
-        s += "Rangee chariots:" + rangeeChariots.toString() + " \n";
-        s += "Rangee rayons: \n";
+        s += "Etat du stock de chariot: " + rangeeChariots.toString() + " \n";
+        s += "Etat du stock de praduits dans les rayons: \n";
         s += rangeeRayons.toString();
-        s += "\nCaisse: \n";
+        s += "\nEtat de la caisse: \n";
         s += caisse.toString() + "\n";
         return s;
     }
